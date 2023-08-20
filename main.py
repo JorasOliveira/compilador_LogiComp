24a95f60a7cd87f86d793a59fec1dbe7b140e7dd
JorasOliveira <joras.custodio@gmail.com>
2023-08-20 12:42:48 -0300

    percebi que estava apenas andando em circulos, sera este passo eu mudo de direcao?

--- main.py
+++ main.py
@@ -7,40 +7,44 @@ args = []
 numbers = []
 
 char_numbers = ['1','2','3','4','5','6','7','8','9','0'] #list to compare the input chars with
-ok_chars = ['+','-',' ','\n','\t',''] #list to compare the input chars with
+ok_chars = ['\n','\t','', ' '] #list to compare the input chars with
+operators = ['+','-']
 
 temp_chars = []
 last_char = strings[0][0]
 
-# 1   -   22+333   +4
-
-
 check_for_numbers = True
+check_for_stupid_annoying_case = False
 
 for str in strings:
     for char in str:
         if char in char_numbers:
-            if last_char in char_numbers or last_char in ok_chars:
+            if check_for_stupid_annoying_case:
+                raise Exception("wrong input")
+
+
+            if last_char in char_numbers or last_char in operators or check_for_numbers:
                 temp_chars.append(char)
 
-            else: 
-                raise Exception("wrong input 1")
-
-        
-        elif char not in ok_chars:
-            raise Exception("wrong input 2")
+        elif (char not in ok_chars) and (char not in operators):
+            raise Exception("wrong input")
         
 
+        if last_char in char_numbers and (char in ok_chars):
+            check_for_stupid_annoying_case = True
+            
         if char == '+':
             args.append(char)
             numbers.append(int(''.join(temp_chars)))
             check_for_numbers = True
+            check_for_stupid_annoying_case = False
             temp_chars = []
 
         if char == '-':
             args.append(char)
             numbers.append(int(''.join(temp_chars)))
             check_for_numbers = True
+            check_for_stupid_annoying_case = False
             temp_chars = []
         
         last_char = char

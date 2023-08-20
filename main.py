08f3b0781c01269f980da2a20c159520c281bad5
JorasOliveira <joras.custodio@gmail.com>
2023-08-20 12:01:30 -0300

    as vezes eu sinto que dou 5 passos pra traz em vez de 1 pra frente, espero que esse seja pra frente

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ char_numbers = ['1','2','3','4','5','6','7','8','9','0'] #list to compare the in
 ok_chars = ['+','-',' ','\n','\t',''] #list to compare the input chars with
 
 temp_chars = []
+last_char = strings[0][0]
+
+# 1   -   22+333   +4
+
 
 check_for_numbers = True
 
 for str in strings:
     for char in str:
         if char in char_numbers:
-
-            if not check_for_numbers:
-                raise Exception("wrong input")
+            if last_char in char_numbers or last_char in ok_chars:
+                temp_chars.append(char)
 
             else: 
-                temp_chars.append(char)
-                check_for_numbers = False
-                check_for_op = True
+                raise Exception("wrong input 1")
+
         
         elif char not in ok_chars:
-            raise Exception("wrong input")
+            raise Exception("wrong input 2")
         
 
         if char == '+':
@@ -41,6 +43,7 @@ for str in strings:
             check_for_numbers = True
             temp_chars = []
         
+        last_char = char
 
 
     numbers.append(int(''.join(temp_chars)))
